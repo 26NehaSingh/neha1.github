70eada155fed06bcf38f3bbe60a54ccdd71c251b
26NehaSingh <35196580+26NehaSingh@users.noreply.github.com>
2018-01-14 16:14:40 +0530

    Create sumoffirst2.c

---
+++ sumoffirst2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int N,K,i,a=1,sum=0,arr[5];
+printf("enter the array size and number\n");
+scanf("%d%d",&N,&K);
+arr[N];
+for(i=1;i<=N;i++){
+	arr[i]=a;
+	a++;
+}
+for(i=1;i<=K;i++)
+sum=sum+arr[i];
+printf("%d\n",sum);
+}
